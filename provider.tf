474fc77bf8dfc22f6c9e0f392d8d56ff1b0e0a89
esiddiqi <siddiquiemaad@gmail.com>
2024-02-14 14:17:03 +0530

    hidden keys

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 //provider "aws" {
 //  region     = "us-west-1"
-//  access_key = "AKIAYS2NVWKHUJ2OULMU"
-//  secret_key = "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"
+//  access_key = "AKIXXXXXXXXXXX"
+//  secret_key = "pAVriXXXXXXXXXXXXP"
 //}
 
